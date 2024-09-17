287acdb9e31a0a7b4d27223d34f3e49006002d5d
Christopher Moore <christopher.e.moore@gmail.com>
2024-09-16 21:01:24 -0700

    automatic or manual

--- resize-images.lua
+++ resize-images.lua
@@ -1,7 +1,18 @@
 #!/usr/bin/env luajit
+local path = require 'ext.path'
 local Image = require 'image'
-for i=0,7 do
-	local img = Image('map-tex-region-'..i..'.png')
-	img = img:resize(img.width/32, img.height/32)
-	img:save('map-tex-region-'..i..'-small.png')
+local function shrink(fn)
+	local img = Image(fn)
+	img = img:resize(img.width/32, img.height/32)	-- from 256x256 pixels to 8x8 pixels
+	local basename = path(fn):getext()
+	img:save(basename..'-small.png')
+end
+
+local fn = ...
+if fn then
+	shrink(fn)
+else
+	for i=0,7 do
+		shrink('map-tex-region-'..i..'.png')
+	end
 end
